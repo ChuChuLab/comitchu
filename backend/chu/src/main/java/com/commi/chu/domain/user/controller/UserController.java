e66a1042583be2a99d4cb00c98c999786898cf33
이은지 <dmswldk28@gmail.com>
2025-07-17 17:07:56 +0900

    fix: dev test

--- backend/chu/src/main/java/com/commi/chu/domain/user/controller/UserController.java
+++ backend/chu/src/main/java/com/commi/chu/domain/user/controller/UserController.java
@@ -16,14 +16,9 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/test")
-    public ResponseEntity<CommonResponse<String>> test(
-            @AuthenticationPrincipal UserPrincipal userPrincipal
-    ) {
-        // 1. userPrincipal 객체에서 사용자 ID를 꺼냅니다.
-        Integer userId = userPrincipal.getId();
+    public ResponseEntity<CommonResponse<String>> test() {
 
-        // 2. 받아온 ID를 사용해서 응답 메시지를 만듭니다.
-        String response = "인증 성공! 당신은 " + userId + "번 유저입니다. 고-수";
+        String response = "인증 성공! 당신은 고-수";
 
         return CommonResponse.ok(response);
     }
