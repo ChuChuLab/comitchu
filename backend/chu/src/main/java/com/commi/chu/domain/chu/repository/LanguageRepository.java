fe3d5b2d5e2228defac2d6e538381916b82cddd4
이은지 <dmswldk28@gmail.com>
2025-08-04 15:23:30 +0900

    feat: 회원가입 시 user_lang에 comit 저장 로직 추가
    - test api 삭제
    - 배경 이미지 업로드

---
+++ backend/chu/src/main/java/com/commi/chu/domain/chu/repository/LanguageRepository.java
@@ -0,0 +1,7 @@
+package com.commi.chu.domain.chu.repository;
+
+import com.commi.chu.domain.chu.entity.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LanguageRepository extends JpaRepository<Language, Integer> {
+}
---
+++ backend/chu/src/main/java/com/commi/chu/domain/chu/repository/UserLangRepository.java
@@ -0,0 +1,7 @@
+package com.commi.chu.domain.chu.repository;
+
+import com.commi.chu.domain.chu.entity.UserLang;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserLangRepository extends JpaRepository<UserLang, Integer> {
+}
