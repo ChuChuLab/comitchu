4fa7dd7eb2d35141ad138c16a3c6b7ef2af4db2b
김형표 <jmn9798@naver.com>
2025-08-04 18:57:29 +0900

    refactor: 언어 이름 및 이미지  대소문자 변경

--- backend/chu/src/main/resources/data.sql
+++ backend/chu/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO user (created_at, github_id, updated_at, github_username, avatar_url)
-VALUES (NOW(), 123123, NOW(), 'testman', 'https://test.com/u/000000000?v=0');
+VALUES (NOW(), 123123, NOW(), 'ramge132', 'https://test.com/u/000000000?v=0');
 
 INSERT INTO language (lang)
-VALUES ('comit'), ('java'), ('python'), ('js'), ('typescript'), ('c'), ('c++'), ('c#'), ('swift'), ('go');
+VALUES ('comit'), ('Java'), ('Python'), ('JavaScript'), ('TypeScript'), ('C'), ('C++'), ('C#'), ('Swift'), ('Go');
 
 INSERT INTO chu (created_at, exp, user_id, name, level, status, lang, background, last_status_updated_at)
 VALUES (NOW(), 0, 1, '커밋츄', 15, 'NORMAL', 'comit', 'flower', NOW());
